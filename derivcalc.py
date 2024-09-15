176b4f4162a94a4685fd08f0b282d51f00c417a1
PatafixPLTX <gaspard.lebaube@gaskam.com>
2024-09-15 17:12:27 +0200

    Implement derivative calculator
    Co-authored-by: kamil-leys <kamil-leys@users.noreply.github.com>

--- derivcalc.py
+++ derivcalc.py
@@ -1,5 +1,5 @@
 PRECEDENCES = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
-FUNCTIONS = ('sin', 'cos', 'log', 'exp', 'sqrt', 'ctg', 'neg')
+FUNCTIONS = ('sin', 'cos', 'tan', 'log', 'exp', 'sqrt', 'ctg', 'neg')
 OPERATORS = ('+', '-', '*', '/', '^')
 
 derivat = []
@@ -115,11 +115,10 @@ def LGB(postfix: list, index: int) -> int:
     i = index
     while remaining > 0:
         remaining -= 1
-        match postfix[i]:
-            case _ if postfix[i] in FUNCTIONS:
-                remaining += 1
-            case _ if postfix[i] in OPERATORS:
-                remaining += 2
+        if postfix[i] in FUNCTIONS:
+            remaining += 1
+        elif postfix[i] in OPERATORS:
+            remaining += 2
         i -= 1
 
     return i + 1
@@ -128,187 +127,224 @@ def GR(postfix: list, index: int) -> int:
     return index - LGB(postfix, index)
 
 def derivative(lwb: int, upb: int, postfix: list) -> list:
+    global derivat
     if postfix[upb] in FUNCTIONS:
-        match postfix[upb]:
-            case 'sin':
-                derivat.extend(postfix[lwb:upb])
-                derivat.append("cos")
-                if postfix[upb - 1] != "0":
-                    derivative(lwb, upb-1, postfix)
-                    derivat.append("*")
-            case 'cos':
-                if postfix[upb - 1] != "0":
-                    derivat.extend(postfix[lwb:upb])
-                    derivat.append("sin")
-                    derivative(lwb, upb-1, postfix)
-                    derivat.extend(("neg", "*"))
-                else:
-                    derivat.append("0")
-            case 'log':
-                assert postfix[upb - 1] != "0" # Logarithm of zero
-
+        if postfix[upb] == 'sin':
+            derivat.extend(postfix[lwb:upb])
+            derivat.append("cos")
+            if postfix[upb - 1] != "0":
                 derivative(lwb, upb-1, postfix)
+                derivat.append("*")
+        elif postfix[upb] == 'cos':
+            if postfix[upb - 1] != "0":
                 derivat.extend(postfix[lwb:upb])
-                derivat.append("/")
-            case 'exp':
+                derivat.append("sin")
                 derivative(lwb, upb-1, postfix)
+                derivat.extend(("neg", "*"))
+            else:
+                derivat.append("0")
+        elif postfix[upb] == 'log':
+            assert postfix[upb - 1] != "0" # Logarithm of zero
+            derivative(lwb, upb-1, postfix)
+            derivat.extend(postfix[lwb:upb])
+            derivat.append("/")
+        elif postfix[upb] == 'exp':
+            derivative(lwb, upb-1, postfix)
+            derivat.extend(postfix[lwb:upb])
+            derivat.extend(("exp", "*"))
+        elif postfix[upb] == 'sqrt':
+            assert postfix[upb - 1] != "0" # Logarithm of zero
+            if postfix[upb - 1] == "1":
                 derivat.extend(postfix[lwb:upb])
-                derivat.extend(("exp", "*"))
-            case 'sqrt':
-                assert postfix[upb - 1] != "0" # Logarithm of zero
-
-                if postfix[upb - 1] == "1":
-                    derivat.extend(postfix[lwb:upb])
-                    derivat.extend(("2", "/"))
-                else:
-                    derivative(lwb, upb-1, postfix)
-                    derivat.append("2")
-                    derivat.extend(postfix[lwb:upb])
-                    derivat.extend(("sqrt", "*", "/"))
-            case 'neg':
+                derivat.extend(("2", "/"))
+            else:
                 derivative(lwb, upb-1, postfix)
-                if derivat[-1] != "0":
-                    derivat.append("neg")
+                derivat.append("2")
+                derivat.extend(postfix[lwb:upb])
+                derivat.extend(("sqrt", "*", "/"))
+        elif postfix[upb] == 'neg':
+            derivative(lwb, upb-1, postfix)
+            if derivat[-1] != "0":
+                derivat.append("neg")
     elif postfix[upb] in OPERATORS:
-        middle = LGB(postfix, upb) + 1
-        match postfix[upb]:
-            case '+' | '-':
-                derivative(lwb, middle-1, postfix)
-                prev_index = len(derivat) - 1
-                derivative(middle, upb-1, postfix)
-
-                index = len(derivat) - 1
-
-                if derivat[prev_index] == "0" and derivat[index] == "0":
-                    derivat.pop()
-                elif derivat[prev_index] == "0":
-                    derivat.pop(prev_index)
-                elif derivat[index] == "0":
-                    derivat.pop(index)
-                    if postfix[upb] == "-":
-                        derivat.append("neg")
-                else:
-                    derivat.append(postfix[upb])
-
-            case '*':
-                firstHalf = ""
-
-                derivat.extend(postfix[lwb:middle])
-                prev_index = len(derivat) - 1
-                derivative(middle, upb - 1, postfix)
-
-                index = len(derivat) - 1
-
-                if derivat[prev_index] == "0" or derivat[index] == "0":
-                    derivat.pop()
-                    derivat.pop()
-                    derivat.append("0")
-                    firstHalf = "0"
-                elif derivat[prev_index] == "1":
-                    derivat.pop(prev_index)
-                elif derivat[index] == "1":
-                    derivat.pop()
-                else:
-                    derivat.append("*")
-
-                ind1 = len(derivat) - 1
-
-                derivative(lwb, middle-1, postfix)
-                prev_index = len(derivat) - 1
-                derivat.extend(postfix[middle:upb])
-
-                index = len(derivat) - 1
-
-                if derivat[prev_index] == "0" or derivat[index] == "0":
-                    derivat.pop()
-                    derivat.pop()
-                    derivat.append("0")
-                    secondHalf = "0"
-                elif derivat[prev_index] == "1":
-                    derivat.pop(prev_index)
-                    secondHalf = "1"
-                elif derivat[index] == "1":
-                    derivat.pop()
-                    secondHalf = "1"
-                else:
-                    derivat.append("*")
-
-                if firstHalf == "0":
-                    derivat.pop(ind1)
-                elif secondHalf == "0":
-                    derivat.pop()
-                else:
-                    derivat.append("+")
-
-            case '/':
-                assert postfix[upb - 1] != "0" # Division by zero
-
-                firstHalf = ""
-
-                derivat.extend(postfix[lwb:middle])
-                prev_index = len(derivat) - 1
-                derivative(middle, upb - 1, postfix)
-
-                index = len(derivat) - 1
-
-                if derivat[prev_index] == "0" or derivat[index] == "0":
-                    derivat.pop()
-                    derivat.pop()
-                    derivat.append("0")
-                    firstHalf = "0"
-                elif derivat[prev_index] == "1":
-                    derivat.pop(prev_index)
-                elif derivat[index] == "1":
-                    derivat.pop()
-                else:
-                    derivat.append("*")
-
-                ind1 = len(derivat) - 1
-
-                derivative(lwb, middle-1, postfix)
-                prev_index = len(derivat) - 1
-                derivat.extend(postfix[middle:upb])
-
-                index = len(derivat) - 1
-
-                if derivat[prev_index] == "0" or derivat[index] == "0":
-                    derivat.pop()
-                    derivat.pop()
-                    derivat.append("0")
-                    secondHalf = "0"
-                elif derivat[prev_index] == "1":
-                    derivat.pop(prev_index)
-                    secondHalf = "1"
-                elif derivat[index] == "1":
-                    derivat.pop()
-                    secondHalf = "1"
-                else:
-                    derivat.append("*")
-
-                if firstHalf == "0":
-                    derivat.pop(ind1)
+        middle = LGB(postfix, upb - 1)
+        if postfix[upb] == '+' or postfix[upb] == '-':
+            derivative(lwb, middle - 1, postfix)
+            prev_index2 = len(derivat) - 1
+            derivative(middle, upb-1, postfix)
+            index = len(derivat) - 1
+            if derivat[prev_index2] == "0" and derivat[index] == "0":
+                derivat.pop()
+            elif derivat[prev_index2] == "0":
+                derivat.pop(prev_index2)
+            elif derivat[index] == "0":
+                derivat.pop(index)
+                if postfix[upb] == "-":
                     derivat.append("neg")
-                elif secondHalf == "0":
+            else:
+                derivat.append(postfix[upb])
+
+        elif postfix[upb] == '*':
+            Zeros = 0
+
+            prev_index1 = len(derivat) - 1
+            derivat.extend(postfix[lwb:middle])
+            prev_index2 = len(derivat) - 1
+            derivative(middle, upb - 1, postfix)
+
+            index = len(derivat) - 1
+
+            if derivat[prev_index2] == "0" or derivat[index] == "0":
+                derivat = derivat[:prev_index1 + 1]
+                Zeros += 1
+            elif derivat[prev_index2] == "1":
+                derivat.pop(prev_index2)
+            elif derivat[index] == "1":
+                derivat.pop()
+            else:
+                derivat.append("*")
+
+            ind1 = len(derivat) - 1
+
+            prev_index1 = len(derivat) - 1
+            derivative(lwb, middle-1, postfix)
+            prev_index2 = len(derivat) - 1
+            derivat.extend(postfix[middle:upb])
+
+            index = len(derivat) - 1
+
+            if derivat[prev_index2] == "0" or derivat[index] == "0":
+                derivat = derivat[:prev_index1 + 1]
+                Zeros += 1
+            elif derivat[prev_index2] == "1":
+                derivat.pop(prev_index2)
+            elif derivat[index] == "1":
+                derivat.pop()
+            else:
+                derivat.append("*")
+
+            if Zeros == 0:
+                derivat.append("+")
+
+        elif postfix[upb] == '/':
+            assert postfix[upb - 1] != "0" # Division by zero
+
+            Zeros = 0
+            firstHalf = ""
+
+            prev_index1 = len(derivat) - 1
+            derivat.extend(postfix[lwb:middle])
+            prev_index2 = len(derivat) - 1
+            derivative(middle, upb - 1, postfix)
+
+            index = len(derivat) - 1
+
+            if derivat[prev_index2] == "0" or derivat[index] == "0":
+                derivat = derivat[:prev_index1 + 1]
+                firstHalf = "0"
+                Zeros += 1
+            elif derivat[prev_index2] == "1":
+                derivat.pop(prev_index2)
+            elif derivat[index] == "1":
+                derivat.pop()
+            else:
+                derivat.append("*")
+
+            ind1 = len(derivat) - 1
+
+            prev_index1 = len(derivat) - 1
+            derivative(lwb, middle-1, postfix)
+            prev_index2 = len(derivat) - 1
+            derivat.extend(postfix[middle:upb])
+
+            index = len(derivat) - 1
+
+            if derivat[prev_index2] == "0" or derivat[index] == "0":
+                derivat = derivat[:prev_index1 + 1]
+                Zeros += 1
+            elif derivat[prev_index2] == "1":
+                derivat.pop(prev_index2)
+            elif derivat[index] == "1":
+                derivat.pop()
+            else:
+                derivat.append("*")
+
+            if firstHalf == "0":
+                derivat.append("neg")
+            elif not derivat[-1] == "0":
+                derivat.append("-")
+
+            if postfix[upb - 1] != "1":
+                derivat.extend(postfix[middle:upb])
+                derivat.extend(("2", "^", "/"))
+
+        elif postfix[upb] == '^':
+            if postfix[upb - 1] == "0" or postfix[middle - 1] == "0":
+                derivat.append("0")
+            elif postfix[middle - 1] == "1":
+                derivat.append("0")
+            else:
+                derivat.extend(postfix[middle:upb])
+                
+                derivative(lwb, middle - 1, postfix)
+
+                if derivat[-1] == "1":
                     derivat.pop()
                 else:
-                    derivat.append("-")
+                    derivat.append("*")
 
-                if postfix[upb - 1] != "1":
-                    derivat.extend(postfix[middle:upb])
-                    derivat.extend(("^", "2", "/"))
+                derivat.extend(postfix[lwb:upb])
+                derivat.extend(("1", "-", "^", "*"))    
 
     else:
         if postfix[upb] == "x":
             derivat.append("1")
         else:
             derivat.append("0")               
-        
-if __name__ == "__main__":
-    expression = input()
-    tokens = tokenize(expression)
-    tokens_with_implicit_mult = add_implicit_multiplication(tokens)
-    postfix = shunting_yard(tokens_with_implicit_mult)
-    print(postfix)
 
-    derivative(0, len(postfix)-1, postfix)
-    print(derivat)
+# def simplify(derivat: list) -> list:
+#     pass
+
+def algebric_notation(derivat: list) -> str:
+    stack = []
+    for token in derivat:
+        if token in OPERATORS:
+            b = stack.pop()
+            a = stack.pop()
+            if token == '+' or token == '-':
+                stack.append(a + " " +  token + " " + b)
+            elif token == '*':
+                stack.append(a + "*" + b)
+            elif token == '/':
+                stack.append(a + "/" + b)
+            elif token == '^':
+                stack.append(a + "^" + b)
+        elif token in FUNCTIONS:
+            if token == 'neg':
+                a = stack.pop()
+                stack.append("-" + a)
+            else:
+                a = stack.pop()
+                stack.append(token + "(" + a + ")")
+        else:
+            stack.append(token)
+    
+    return stack[0] if stack else ""
+
+def credits():
+    print("Derivative Calculator © 2024 Gaskam.com - All rights reserved.\nProgrammed by Leys Kamil and Lebaube Gaspard - github.com/gaskam-com\nInspired by 'SYMBOLIC DERIVATION WITHOUT USING EXPRESSION TREES (2001)' by V. KRTOLICA and S. STANIMIROVIC\n\nFor more information, visit github.com/gaskam-com/derivcalc\n\nEnjoy! :)\n")
+
+def main():
+    credits()
+    while True:
+        expression = input("Expression: ")
+        tokens = tokenize(expression)
+        tokens_with_implicit_mult = add_implicit_multiplication(tokens)
+        postfix = shunting_yard(tokens_with_implicit_mult)
+        derivative(0, len(postfix)-1, postfix)
+        # siplified = simplify(derivat)
+        infix_notation = algebric_notation(derivat)
+        print("Derivative: ", infix_notation, "\n\n", 30*'-', "\n")
+        derivat.clear()
+
+main()
